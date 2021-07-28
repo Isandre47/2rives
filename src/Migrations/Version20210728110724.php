999a638b871761d737ad0bca8bd90552885764f4
Isandre47 <nuanda@live.fr>
2021-07-28 13:32:42 +0200

    init project on taffbledard

--- src/Migrations/Version20210728110724.php
+++ src/Migrations/Version20210728110724.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190228094536 extends AbstractMigration
+final class Version20210728110724 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,7 @@ final class Version20190228094536 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE file (id INT AUTO_INCREMENT NOT NULL, document_key VARCHAR(255) NOT NULL, path VARCHAR(255) NOT NULL, deleted TINYINT(1) DEFAULT \'0\' NOT NULL, INDEX document_key_idx (document_key), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE category ADD jour VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE emission CHANGE medias medias VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -32,8 +30,6 @@ final class Version20190228094536 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE file');
         $this->addSql('ALTER TABLE category DROP jour');
-        $this->addSql('ALTER TABLE emission CHANGE medias medias VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci');
     }
 }
